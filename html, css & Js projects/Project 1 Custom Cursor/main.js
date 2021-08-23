8ddabf76b38afbd55e42b0cbdbbf76fe208ca5a2
RiazMehmood <57244218+RiazMehmood@users.noreply.github.com>
2021-08-23 20:37:38 +0500

    new changes

---
+++ html, css & Js projects/Project 1 Custom Cursor/main.js
@@ -0,0 +1,17 @@
+// const cursor = document.querySelector('.cursor');
+
+// window.addEventListener('mouseover', (e)=> {
+//     // console.log(e.pageX, e.pageY);
+//     cursor.style.left = e.pageX + "px";
+//     cursor.style.top = e.pageY + "px";
+//     // cursor.setAttribute('distance-from-top', (cursor.offsetTop - cursor.scroll))
+//     console.log(e)
+
+// })
+
+const value1 = document.getElementsByName("input").value;
+const value2 = document.getElementsByName("val2").value;
+const calc = document.getElementsByName("button").value;
+
+
+console.log(value1, value2, calc);
